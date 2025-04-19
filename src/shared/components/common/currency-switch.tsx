b486c17de3f52c0782beca09e9b77ea7baed8de6
황성재 <hwangsungjae@hwangseongjaeui-Macmini.local>
2025-04-19 18:02:51 +0900

    feat: implement currency-switch common component

--- src/shared/components/common/currency-switch.tsx
+++ src/shared/components/common/currency-switch.tsx
@@ -1,12 +1,14 @@
-import React from "react";
 import { Switch } from "../ui/switch";
-import { Label } from "../ui/label";
 
-function CurrencySwitch(props) {
+interface Props {
+  leftText: string;
+  rightText: string;
+}
+
+function CurrencySwitch({ leftText, rightText }: Props) {
   return (
     <div className="flex items-center space-x-2">
-      <Switch id="airplane-mode" />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      <Switch leftText={leftText} rightText={rightText} />
     </div>
   );
 }
--- src/shared/components/ui/switch.tsx
+++ src/shared/components/ui/switch.tsx
@@ -3,23 +3,40 @@ import * as SwitchPrimitive from "@radix-ui/react-switch";
 
 import { cn } from "@/shared/lib/utils";
 
+interface Props {
+  leftText?: string;
+  rightText?: string;
+}
+
 function Switch({
   className,
+  leftText,
+  rightText,
   ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+}: React.ComponentProps<typeof SwitchPrimitive.Root> & Props) {
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "relative group peer cursor-pointer p-0.5 data-[state=checked]:bg-background-toss-secondary data-[state=unchecked]:bg-background-toss-secondary rounded-[6px] focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[26px] w-12 shrink-0 items-center border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 border-none m-0",
         className
       )}
       {...props}
     >
+      {leftText && rightText && (
+        <div className="flex text-toss-sm text-foreground-toss font-semibold absolute z-10">
+          <p className="w-[22px] h-[22px] group-data-[state=checked]:text-foreground-toss-secondary flex-center">
+            $
+          </p>
+          <p className="w-[22px] h-[22px] group-data-[state=unchecked]:text-foreground-toss-secondary flex-center">
+            원
+          </p>
+        </div>
+      )}
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+          "bg-background-toss-tertiary rounded-[4px] block size-[22px] ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%)] data-[state=unchecked]:translate-x-0"
         )}
       />
     </SwitchPrimitive.Root>
